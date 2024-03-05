5674b0dcfb24b3bb8c287359051b8b20524865ab
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2024-03-05 18:51:37 +0500

    Working Inprogress

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      Aboutttt
+    </div>
+  )
+}
+
+export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { ReactTyped } from 'react-typed'
 
 const Home = () => {
   return (
-    <div className='w-[100vw] h-[100vh] flex justify-center items-center'>
-      <h1 className='text-white'>Hi.</h1>
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center home'>
+      <div className='flex flex-col'>
+        <ReactTyped
+          strings={[
+            "Hi. 👋",
+          ]}
+          typeSpeed={40}
+          showCursor={false}
+          className='text-white text-2xl text-center'
+        >
+        </ReactTyped>
+        <ReactTyped
+          strings={[
+            "I am Syed Umair.",
+          ]}
+          startDelay={400}
+          typeSpeed={40}
+          showCursor={false}
+          className='text-white text-2xl'
+        >
+        </ReactTyped>
+        <button>
+          <span>About Me</span>
+        </button>
+
+      </div>
     </div>
   )
 }
